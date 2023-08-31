9a5a2f3904598900413ac7ba283d3ff605f2972e
Mirsaid04 <mirsaidd04@gmail.com>
2023-08-31 11:01:27 +0900

    Add-New-Classes

---
+++ Classes/Calculation.cs
@@ -0,0 +1,49 @@
+namespace averagely.Classes;
+
+public class Calculation
+{
+    public  decimal OverallCalculation()
+    {
+        Console.Write("History: ");
+        decimal history=Convert.ToDecimal(Console.ReadLine());
+
+        Console.Write("motherTongue: ");
+        decimal motherTongue=Convert.ToDecimal(Console.ReadLine());
+
+        Console.Write("math: ");
+        decimal math=Convert.ToDecimal(Console.ReadLine());
+
+        Console.Write("biology: ");
+        decimal biology=Convert.ToDecimal(Console.ReadLine());
+
+        Console.Write("chemistry: ");
+        decimal chemistry=Convert.ToDecimal(Console.ReadLine());
+            
+        decimal result=Convert.ToDecimal(motherTongue+history+math+biology+chemistry);
+        Console.WriteLine("Your overall grade is " + result);
+        Console.WriteLine();
+        return result;
+    }
+    public decimal AverageCalculation()
+    {
+        Console.Write("History: ");
+        double averageHistory=Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("motherTongue: ");
+        double averageMotherTongue=Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("math: ");
+        double averageMath=Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("biology: ");
+        double averageBiology=Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("chemistry: ");
+        double averageChemistry=Convert.ToDouble(Console.ReadLine());
+     
+        decimal averageResult=Convert.ToDecimal(averageHistory+averageMotherTongue+averageMath+averageBiology+averageChemistry);
+        Console.WriteLine("Your average grade is " + averageResult/5); 
+        Console.WriteLine();
+        return averageResult;
+    }
+}
---
+++ Classes/Reporter.cs
@@ -0,0 +1,19 @@
+namespace averagely.Classes;
+
+public class Reporter
+{
+    public void ShowMenu()
+    {
+        Console.WriteLine("\n<---Hello , Welcome to our university---->\n");
+        Console.WriteLine("Do you want to know your overall and average score?\n\n1.Overall\n2.Average\n ");
+        Console.Write($"You should choose one of them {'1'} or {'2'} : ");
+    }
+    public void ShowGratitude()
+    {
+        Console.WriteLine(" --Thank you-- \n"); 
+    }
+    public void ReportLastInfo()
+    {
+        Console.WriteLine("Sorry , you entered invalid input\n<---Try again later--->");
+    }
+}
